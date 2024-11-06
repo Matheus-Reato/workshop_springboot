3f7d36eb6e0647795b72dee9bdc78284506d92f4
Matheus Reato <matheusreato13@gmail.com>
2024-11-06 15:10:33 -0300

    User update

--- src/main/java/com/cursoSpring/course/resources/UserResource.java
+++ src/main/java/com/cursoSpring/course/resources/UserResource.java
@@ -19,7 +19,6 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-
         List<User> list = service.findAll();
 
         return ResponseEntity.ok().body(list);
@@ -35,16 +34,23 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
-
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);
 
+        return ResponseEntity.created(uri).body(obj);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
+
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = service.update(id, obj);
+
+        return ResponseEntity.ok().body(obj);
     }
 
 }
