283688aefaa549c7683e3bb810682d8c0e3657c3
Matheus Reato <matheusreato13@gmail.com>
2024-11-06 10:58:26 -0300

    OrderItem, many-to-many association with extra attributes - part 2

--- src/main/java/com/cursoSpring/course/entities/OrderItem.java
+++ src/main/java/com/cursoSpring/course/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package com.cursoSpring.course.entities;
 
 import com.cursoSpring.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -11,7 +12,7 @@ import java.util.Objects;
 public class OrderItem implements Serializable {
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
     private Integer quantity;
     private Double price;
 
@@ -41,6 +42,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder(){
         return id.getOrder();
     }
